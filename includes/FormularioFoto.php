b3f5761583cba89f66003b5559b90e5096176580
A-Ortiz-L <wilortiz@ucm.es>
2017-06-15 13:15:15 +0200

    Create FormularioFoto.php

--- includes/FormularioFoto.php
+++ includes/FormularioFoto.php
@@ -43,8 +43,8 @@ EOF;
     }
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
+        unlink($target_file);
+        $uploadOk = 1;
     }
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 500000) {
