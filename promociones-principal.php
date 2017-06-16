57b9aa13801ce4164eaf80c92bb729dd2be3356c
Alejandromndza <alejme02@ucm.es>
2017-06-16 13:37:40 +0200

    Add files via upload

--- promociones-principal.php
+++ promociones-principal.php
@@ -29,17 +29,17 @@ require_once __DIR__.'/includes/config.php';
           	<h1 class="promotitulo"> Promociones destacadas </h1>
 
             <div class="promos-img">
-               <a href="http://localhost/miweb/promociones-detalle1.php"><img src="IMG/promo1.jpg"></a>
+               <a href="promociones-detalle1.php"><img src="IMG/promo1.jpg"></a>
                <p class ="descripdes">Jueves mas barato</p>
             </div>
 
             <div class="promos-img">
-               <a href="http://localhost/miweb/promociones-detalle2.php"><img src="IMG/promo2.jpg"></a>
+               <a href="promociones-detalle2.php"><img src="IMG/promo2.jpg"></a>
               <p class ="descripdes">Cambia tus puntos</p>
             </div>
 
             <div class="promos-img">
-               <a href="http://localhost/miweb/promociones-detalle3.php"><img src="IMG/promo3.jpg"></a>
+               <a href="promociones-detalle3.php"><img src="IMG/promo3.jpg"></a>
               <p class ="descripdes">Euro 6000 menú</p>
             </div>
 
@@ -55,30 +55,30 @@ require_once __DIR__.'/includes/config.php';
           	<h1 class="promotitulo"> Todas nuestras promociones </h1>
 
             <div class="promos-img">
-               <a href="http://localhost/miweb/promociones-detalle1.php"><img src="IMG/promo1.jpg"></a>
+               <a href="promociones-detalle1.php"><img src="IMG/promo1.jpg"></a>
                <p class ="descripdes">Jueves mas barato</p>
 
             </div>
 
 
             <div class="promos-img">
-               <a href="http://localhost/miweb/promociones-detalle2.php"><img src="IMG/promo2.jpg"></a>
+               <a href="promociones-detalle2.php"><img src="IMG/promo2.jpg"></a>
               <p class ="descripdes">Cambia tus puntos</p>
             </div>
 
             <div class="promos-img">
-              <a href="http://localhost/miweb/promociones-detalle3.php"><img src="IMG/promo3.jpg"></a>
+              <a href="promociones-detalle3.php"><img src="IMG/promo3.jpg"></a>
               <p class ="descripdes">Euro 6000 menú</p>
             </div>
 
             <div class="promos-img">
-               <a href="http://localhost/miweb/promociones-detalle4.php"><img src="IMG/promo4.jpg"></a>
+               <a href="promociones-detalle4.php"><img src="IMG/promo4.jpg"></a>
                <p class ="descripdes">La gran quedada</p>
 
             </div>
 
             <div class="promos-img">
-               <a href="http://localhost/miweb/promociones-detalle5.php"><img src="IMG/promo5.jpg"></a>
+               <a href="promociones-detalle5.php"><img src="IMG/promo5.jpg"></a>
               <p class ="descripdes">Miércoles al cine</p>
             </div>
 
