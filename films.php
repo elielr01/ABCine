ab7e571f18eeb6cda9f04a34246cd45b93a63184
Alejandromndza <alejme02@ucm.es>
2017-06-16 13:05:33 +0200

    Add files via upload

--- films.php
+++ films.php
@@ -20,99 +20,26 @@ require_once __DIR__.'/includes/config.php';
 		$app->doInclude('comun/header.php');
 		?>
 		<article>
-			<div class="pelicula" >
-				<a href="pelicula-detalle.php"> <img  src="IMG/kkong.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a  href="pelicula-detalle.php"><img src="IMG/mult.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/life.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a href="#"><img src="IMG/logan.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/zonahostil.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/byb.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/ff.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/eptb.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/cb.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/gi.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/muralla.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/cantabrico.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/bat.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/power.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/babyb.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/memp.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/canta.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/aliados.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/bar.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/pitufos.jpg"/></a>
-			</div>
+		
+		<?php
+      $conn = $app->conexionBd();
+      $sql = "SELECT *
+              FROM Pelicula";
+             
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+        // output data of each row
 
-			<div class="pelicula">
-				<a href="#"><img src="IMG/rey.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a href="#"><img src="IMG/gits.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/GGvol2.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a href="ww.html"><img src="IMG/ww.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				<a href="#"><img src="IMG/jl.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a href="#"><img src="IMG/swviii.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a href="#"><img src="IMG/pcaribe.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a href="#"><img src="IMG/thorrag.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a href="#"><img src="IMG/br2049.jpg"/></a>
-			</div>
-			<div class="pelicula">
-				 <a href="#"><img src="IMG/spiderm.jpg"/></a>
-			</div>
-			<div class="pelicula">
-			</div>
+        while($row = $result->fetch_assoc()) {
+
+          echo "<div class='pelicula' >";
+          echo "<a href='pelicula-detalle.php?id=".$row['id']."'> <img src= 'IMG/".$row['nombreFoto']."'/></a>";
+          echo "</div>";
+        }
+      }
+      ?>
+		
+			
 		</article>
 
     <?php
