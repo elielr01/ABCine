bdf000a23b3f66ce5f445f6ef38fe9e81480293d
A-Ortiz-L <wilortiz@ucm.es>
2017-06-16 11:17:17 +0200

    update reg.php

--- registro.php
+++ registro.php
@@ -31,7 +31,10 @@ require_once __DIR__.'/includes/config.php';
 		$formRegistro->gestiona();
 		?>
 
-		</article>
+			
+		<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="js/checkuser.js"></script>
+
 
 
     <?php
