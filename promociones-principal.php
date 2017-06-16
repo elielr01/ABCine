13e6d1fc763c63efad1ab97985e0fe82085dca27
Alejandromndza <alejme02@ucm.es>
2017-06-16 12:35:37 +0200

    Add files via upload

--- promociones-principal.php
+++ promociones-principal.php
@@ -24,45 +24,76 @@ require_once __DIR__.'/includes/config.php';
       -->
       <div class="cine-contenedor">
 
-        <div class="cine-contenedor-header">
-        </div>
-
-        <div class="cine-contenedor-promos">
           <div class="promos-destacadas">
-            <h1>Promociones destacadas</h1>
-            <div class="promos-img">
-               <a href="http://localhost/miweb/promociones-detalle.php"><img src="IMG/promo1.jpg"></a>
-            </div>
-            <div class="promos-img">
-              <img src="IMG/promo2.jpg">
-            </div>
-            <div class="promos-img">
-              <img src="IMG/promo3.jpg">
-            </div>
 
-          </div>
+          	<h1 class="promotitulo"> Promociones destacadas </h1>
 
-          <div class="promos">
-            <h1>Todas nuestras las promociones</h1>
             <div class="promos-img">
-              <img src="IMG/promo1.jpg">
-            </div>
-            <div class="promos-img">
-              <img src="IMG/promo2.jpg">
-            </div>
-            <div class="promos-img">
-              <img src="IMG/promo3.jpg">
+               <a href="http://localhost/miweb/promociones-detalle1.php"><img src="IMG/promo1.jpg"></a>
+               <p class ="descripdes">Jueves mas barato</p>
             </div>
 
             <div class="promos-img">
-              <img src="IMG/promo4.jpg">
+               <a href="http://localhost/miweb/promociones-detalle2.php"><img src="IMG/promo2.jpg"></a>
+              <p class ="descripdes">Cambia tus puntos</p>
             </div>
-            <div class="promos-img">
-              <img src="IMG/promo5.jpg">
-            </div>
-          </div>
 
-        </div>
+            <div class="promos-img">
+               <a href="http://localhost/miweb/promociones-detalle3.php"><img src="IMG/promo3.jpg"></a>
+              <p class ="descripdes">Euro 6000 menú</p>
+            </div>
+
+            <marquee class="tit1">ABCINE se reserva el derecho a modificar o cancelar el descuento durante el
+			desarrollo de la misma si concurriesen circunstancias de fuerza mayor o caso fortuito
+			que así lo impusiesen. Para cualquier consulta o problema en el uso de los cupones, puede ponerse en contacto con el Servicio de Atención al Cliente en el 902 453 453</marquee>
+
+       </div>
+
+
+       <div class="todas-promociones">
+
+          	<h1 class="promotitulo"> Todas nuestras promociones </h1>
+
+            <div class="promos-img">
+               <a href="http://localhost/miweb/promociones-detalle1.php"><img src="IMG/promo1.jpg"></a>
+               <p class ="descripdes">Jueves mas barato</p>
+
+            </div>
+
+
+            <div class="promos-img">
+               <a href="http://localhost/miweb/promociones-detalle2.php"><img src="IMG/promo2.jpg"></a>
+              <p class ="descripdes">Cambia tus puntos</p>
+            </div>
+
+            <div class="promos-img">
+              <a href="http://localhost/miweb/promociones-detalle3.php"><img src="IMG/promo3.jpg"></a>
+              <p class ="descripdes">Euro 6000 menú</p>
+            </div>
+
+            <div class="promos-img">
+               <a href="http://localhost/miweb/promociones-detalle4.php"><img src="IMG/promo4.jpg"></a>
+               <p class ="descripdes">La gran quedada</p>
+
+            </div>
+
+            <div class="promos-img">
+               <a href="http://localhost/miweb/promociones-detalle5.php"><img src="IMG/promo5.jpg"></a>
+              <p class ="descripdes">Miércoles al cine</p>
+            </div>
+
+
+            <marquee class="tit1">ABCINE se reserva el derecho a modificar o cancelar el descuento durante el
+			desarrollo de la misma si concurriesen circunstancias de fuerza mayor o caso fortuito
+			que así lo impusiesen. Para cualquier consulta o problema en el uso de los cupones, puede ponerse en contacto con el Servicio de Atención al Cliente en el 902 453 453</marquee>
+
+       </div>
+
+
+
+
+
+
       </div>
 
 		</article>
