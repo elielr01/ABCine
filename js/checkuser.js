cbfde0aeabf44ea97607c6a1ff510f8b3f49a19e
A-Ortiz-L <wilortiz@ucm.es>
2017-06-16 18:14:39 +0200

    Add files via upload

--- js/checkuser.js
+++ js/checkuser.js
@@ -1,17 +1,19 @@
-
-
 $(document).ready(function(){
+	
 	$(".campoEmail").change(function(){
 		
+		
 		var url="includes/comprobarUsuario.php?user=" + $(".campoEmail").val();
+		
 	
 		$.get(url,usuarioExiste,"json");
-	
+			
 
 	});
 });
 
 function usuarioExiste(respuesta){
+		
 		var exp = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/;
 		var correo = $(".campoEmail").val();
 			
