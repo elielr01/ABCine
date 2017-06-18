ca3a325daff079dd774f09b424e8aa69d770b6eb
A-Ortiz-L <wilortiz@ucm.es>
2017-06-18 12:29:25 +0200

    Update checkuser.js

--- js/checkuser.js
+++ js/checkuser.js
@@ -1,44 +1,57 @@
 $(document).ready(function(){
-	
-	$(".campoEmail").change(function(){
-		
-		
-		var url="includes/comprobarUsuario.php?user=" + $(".campoEmail").val();
-		
-	
-		$.get(url,usuarioExiste,"json");
-			
 
+	$(".campoEmail").change(function(){
+
+		verifyUser();
 	});
 });
 
-function usuarioExiste(respuesta){
-		
+function verifyUser(){
+
+		var urlweb="includes/comprobarUsuario.php";
 		var exp = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/;
 		var correo = $(".campoEmail").val();
-			
-		 if ( respuesta.data=="Existe" && exp.test(correo)) {
-			
-			$(".noimg1").css("display", "inline-block");
-		
-			$(".okimg1").css("display", "none");
-		} else if ( respuesta.data=="Existe" && !exp.test(correo) ){
-		
-			$(".noimg1").css("display", "inline-block");
-		
-			$(".okimg1").css("display", "none");
-		
-		}else if ( respuesta.data=="Disponible" && exp.test(correo) ){
-		
-			$(".okimg1").css("display", "inline-block");
-		
-			$(".noimg1").css("display", "none");
-		
-		}else if ( respuesta.data=="Disponible" && !exp.test(correo) ){
-		
-			$(".noimg1").css("display", "inline-block");
-		
-			$(".okimg1").css("display", "none");
-		}
-			
+
+
+		var datos = {
+	    email:correo
+	  };
+
+
+
+		var request = $.ajax({
+	        url: urlweb,
+	        type: "get",
+	        data: datos,
+	        success: function(response) {
+						if ( response=="Existe" && exp.test(correo)) {
+
+ 						$(".noimg1").css("display", "inline-block");
+
+ 						$(".okimg1").css("display", "none");
+ 					} else if ( response=="Existe" && !exp.test(correo) ){
+
+ 						$(".noimg1").css("display", "inline-block");
+
+ 						$(".okimg1").css("display", "none");
+
+ 					}else if ( response=="Disponible" && exp.test(correo) ){
+
+ 						$(".okimg1").css("display", "inline-block");
+
+ 						$(".noimg1").css("display", "none");
+
+ 					}else if ( response=="Disponible" && !exp.test(correo) ){
+
+ 						$(".noimg1").css("display", "inline-block");
+
+ 						$(".okimg1").css("display", "none");
+ 					}
+				},
+
+	        error: function() {
+	          alert('error');
+	        }
+	  });
+
 	}
