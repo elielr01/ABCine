deb43f2e6d9600d1ebf63097c9e272c74c8c0a10
A-Ortiz-L <wilortiz@ucm.es>
2017-06-21 18:03:49 +0200

    Add files via upload

---
+++ includes/querySearchForBan.php
@@ -0,0 +1,24 @@
+<?php
+  namespace es\ucm\fdi\aw;
+  require_once __DIR__.'/config.php';
+
+  $app = App::getSingleton();
+  $conn = $app->conexionBd();
+
+
+  $sql2 = "SELECT u.username FROM usuarios u,rolesusuario r
+          WHERE r.usuario=u.id and r.rol='1'";
+
+
+  $result = $conn->query($sql2);
+
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $var = $row['username'] . " \n";
+      echo $var;
+      $var="";
+
+    }
+  }
+
+  ?>
