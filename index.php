9e98f2b3af40ec896540e2e0996f42120f689263
David Ortiz <davidortiz@ucm.es>
2017-06-22 12:18:47 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -11,14 +11,19 @@ require_once __DIR__.'/includes/config.php';
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="utf-8" />
   <link rel="stylesheet" href="css/footerHeader/estilo.css">
+   <link rel="stylesheet" href="css/goup/fonts.css">
+   <script src="http://code.jquery.com/jquery-latest.js"></script>
+   <script src="js/arriba.js"></script>
   </head>
 
 
 	<body>
+	
+		
 		<?php
 		$app->doInclude('comun/header.php');
 		?>
-
+		<span class="ir-arriba icon-arrow-up2"></span>
 		<article>
 
 			<div class="slide">
