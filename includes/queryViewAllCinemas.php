13fef1401f27aee0e330703fe260c5095d0d444e
Elí Emmanuel Linares Romero <eli.emmanuel01@gmail.com>
2017-06-22 19:24:41 +0200

    Se arregló cine-principal

--- includes/queryViewAllCinemas.php
+++ includes/queryViewAllCinemas.php
@@ -3,30 +3,38 @@
   namespace es\ucm\fdi\aw;
 
   $conn = App::conexionBd();
-  $sql = "SELECT c.nombre as c_nombre, p.nombreFoto, p.nombre as p_nombre, p.sinopsis
-          FROM cine c, sala s, pelicula p
-          WHERE c.id = s.idCine AND s.idPelicula = p.id
-          ORDER BY c.id";
+  $sql = "SELECT c.nombre AS c_nombre, p.id AS p_id, p.nombre AS p_nombre, p.nombreFoto, p.sinopsis, f.fecha
+          FROM cine c, sala s, funcion f, pelicula p
+          WHERE c.id = s.idCine AND s.id = f.id_sala AND f.id_pelicula = p.id
+          GROUP BY c.id, p.id
+          ORDER BY c.id, p.id
+          ";
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
     // output data of each row
-    $nombre_cine = "";
-    echo "<div class='cine-contenedor'>";
+    $nombre_cine_anterior = "";
+    $boolCineAbierto = false;
     while($row = $result->fetch_assoc()) {
-      if ($row['c_nombre'] != $nombre_cine) {
-        $nombre_cine = $row['c_nombre'];
-        echo "</div></div>";
+
+      $nombre_cine_ahora = $row['c_nombre'];
+
+      if ($nombre_cine_ahora != $nombre_cine_anterior) {
+        if ($boolCineAbierto) {
+          echo "</div></div>";
+        }
+        $nombre_cine_anterior = $nombre_cine_ahora;
+        $boolCineAbierto = true;
         echo "<div class='cine-contenedor'>";
-        echo "<div class='cine-contenedor-header'><h1>" . $nombre_cine . "</h1></div>";
+        echo "<div class='cine-contenedor-header'><h1>" . $nombre_cine_ahora . "</h1></div>";
         echo "<div class='cine-contenedor-peliculas'>";
       }
       echo "<div class='pelicula-contenedor'><h2>" . $row['p_nombre'] . "</h2>";
-      echo "<div class='pelicula-img'><a href='#'><img src='IMG/" . $row['nombreFoto'] . "'/></a></div>";
+      echo "<div class='pelicula-img'><a href='pelicula-detalle.php?id=" . $row['p_id'] . "'><img src='IMG/" . $row['nombreFoto'] . "'/></a></div>";
       echo "<div class='pelicula-detalles'><p>";
       echo $row['sinopsis'];
-      echo "</p><br><h4>Horarios:</h4><br><p class='horarios'>";
+      //echo "</p><br><h4>Horarios:</h4><br><p class='horarios'>";
       //TODO ESTO HAY CAMBIAR LA TABLA DE FUNCION PARA OBTENER LOS HORARIOS.
-      echo "13:30<br>";
+      //echo "13:30<br>";
       echo "</p></div></div>";
     }
     echo "</div></div>";
