adf709c7518a43b3d4d0c56786ddd1c3708de331
Alejandromndza <alejme02@ucm.es>
2017-06-22 22:01:33 +0200

    Add files via upload

--- promociones-detalle.php
+++ promociones-detalle.php
@@ -20,26 +20,11 @@ require_once __DIR__.'/includes/config.php';
 
 		<article id=main>
 
-            <div class="promo-contenedor">
-        <h1>Nombre de la promocion</h1>
-        <div class="promo-img">
-          <img src="IMG/promo-detalle.png">
-        </div>
-        <div class="promo-descripcion">
-          <p>
-            Promocion: Por motivo de nuestro aniversario, te invitamos al cine gratis.<br>
-            3(s): 2 19 de Abril del 2017<br>
-            Cine(s): Todos<br>
-            Detalles:<br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae neque vulputate, faucibus lectus sed,
-            rhoncus nibh. Vestibulum erat ligula, dictum in leo et, pharetra blandit leo. In mattis mauris ut erat
-            elementum cursus. Vestibulum id dui dolor. Proin ac lectus eu eros commodo mattis et sed lacus. Praesent
-            vestibulum consequat ipsum sed dictum. Cras euismod eu massa ut lobortis. Nulla eros tortor, vulputate in
-            elit nec, feugiat convallis dolor. Integer consectetur rutrum eros, eget commodo justo ultricies ac.
-          </p>
-        </div>
-      </div>
-		</article>
+      <?php
+      $app->doInclude('queryViewPromos.php');
+      ?>
+
+    </article>
 
     <?php
 		$app->doInclude('comun/footer.html');
